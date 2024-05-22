646aeae8cea59767311441043e44e4b37fa4ddf7
Karol Jessenia Castro Bedoya <karol.castro@ucp.edu.co>
2024-05-21 19:13:12 -0500

    envio email

---
+++ registro.php
@@ -0,0 +1,40 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('castrokarol312@gmail.com', 'Tienda');
+    $mail->addAddress('Miyarleth.varelas@ucp.edu.co');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Registro Exitoso';
+    $mail->Body    = 'Usuario y Contraseña de ingreso</b>';
+
+    $mail->send();
+    echo 'Enviado Correctamente';
+} catch (Exception $e) {
+    echo "Error al enviar: {$mail->ErrorInfo}";
+}
